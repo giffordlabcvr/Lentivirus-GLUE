f7b5e48dc38997a3927720baac64008a5a09e73f
Rob Gifford <robjgiff@gmail.com>
2020-03-07 17:32:16 +0000

    Validated core build

--- glue/core/lentiLoadRefseqsIsolateData2.js
+++ glue/core/lentiLoadRefseqsIsolateData2.js
@@ -1,22 +1,29 @@
 // Load EVE data from tab file 
 var loadResult;
-glue.inMode("module/flaviviridaeTabularUtility", function() {
-	loadResult = glue.tableToObjects(glue.command(["load-tabular", "tabular/locus/efv-locus-data.tsv"]));
+glue.inMode("module/lentiTabularUtility", function() {
+	loadResult = glue.tableToObjects(glue.command(["load-tabular", "tabular/refseq/lenti-reference-data.tsv"]));
 	//glue.log("INFO", "load result was:", loadResult);
 });
 
-_.each(loadResult, function(eveObj) {
 
-	glue.inMode("custom-table-row/locus_data/"+eveObj.sequenceID, function() {
+_.each(loadResult, function(refSeqObj) {
+
+    glue.log("INFO", "refSeqObj was:", refSeqObj);
+	glue.inMode("custom-table-row/isolate_data/"+refSeqObj.sequenceID, function() {
 	
-		glue.command(["set", "field", "scaffold", eveObj.scaffold]);
-		glue.command(["set", "field", "start", eveObj.start]);
-		glue.command(["set", "field", "end", eveObj.end]);
-		glue.command(["set", "field", "orientation", eveObj.orientation]);
-		glue.command(["set", "field", "length", eveObj.length]);
-		glue.command(["set", "field", "integration", eveObj.integration]);
-		glue.command(["set", "field", "fragment", eveObj.fragment]);
-		glue.command(["set", "field", "duplicate", eveObj.duplicate]);
+		glue.command(["set", "field", "host_sci_name", refSeqObj.host_sci_name]);
+		glue.command(["set", "field", "host_common_name", refSeqObj.host_common_name]);
 
 	});
+
+	glue.inMode("sequence/ncbi-refseqs/"+refSeqObj.sequenceID, function() {
+	
+		glue.command(["set", "field", "name", refSeqObj.name]);
+		glue.command(["set", "field", "full_name", refSeqObj.full_name]);
+		glue.command(["set", "field", "subgenus", refSeqObj.subgenus]);
+		glue.command(["set", "field", "clade", refSeqObj.clade]);
+		glue.command(["set", "field", "subtype", refSeqObj.subtype]);
+	});
+
+
 });
