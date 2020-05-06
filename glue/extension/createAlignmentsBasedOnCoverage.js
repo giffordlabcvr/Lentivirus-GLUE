36b455357e5c12b7316e024679b604b38e44f287
Robert Gifford <robjgiff@gmail.com>
2020-05-06 06:21:45 +0100

    Script for constructing gene-level alignments based on coverage (floc_note table)

--- glue/extension/createAlignmentsBasedOnCoverage.js
+++ glue/extension/createAlignmentsBasedOnCoverage.js
@@ -5,11 +5,11 @@ var where_clause = "name not like '%root%'";
 var alignmentArray = get_alignments(where_clause);
 
  // Iterate through the alignments
-var mySpeciesAligns = {};
 for(var k = 0; k < alignmentArray.length; k++) {
 
-	// get the reference sequence 
+	// Get the reference sequence
 	var alignmentData = alignmentArray[k];
+	glue.logInfo("Processing alignmentData: "+alignmentData);
 	var refSeqName = alignmentData["refSequence.name"];
 	var alignmentName = alignmentData["name"];
 	glue.logInfo("Processing reference seq: "+refSeqName);
@@ -18,32 +18,38 @@ for(var k = 0; k < alignmentArray.length; k++) {
 	var listFeatureResult = get_refseq_feature_locations(refSeqName);
 
 	// get sequences with above threshold coverage for each feature
-	var myGeneAligns = {};
     for(var j = 0; j < listFeatureResult.length; j++) {
 
-	   var featureName = listFeatureResult[j];
-	   glue.logInfo("  Processing feature: "+featureName);
-
-	   // get the feature locations
-	   var listFeatureResult;
-	   glue.inMode("/reference/"+refSeqName, function() {
-		   listFeatureResult = glue.command(["list", "feature-location"]);
-	   });
+		var featureName = listFeatureResult[j];
+		glue.logInfo("Processing feature: "+featureName);
+			
+		// Get the members with sufficient coverage for this feature
+		var sufficientCoverageMembers;
+		glue.inMode("alignment/"+alignmentName+"/", function(){			
+			sufficientCoverageMembers = glue.getTableColumn(glue.command(["list", "member", "-w", "fLocNotes.featureLoc.feature.name = 'Env' and fLocNotes.ref_nt_coverage_pct >= "+threshold]), "sequence.sequenceID");
+		});		   
 	   
-	   var sufficientCoverageMembers;
-	   glue.inMode("alignment/"+alignmentName+"/", function(){			
-		   sufficientCoverageMembers = glue.getTableColumn(glue.command(["list", "member", "-w", "fLocNotes.featureLoc.feature.name = 'Env' and fLocNotes.ref_nt_coverage_pct >= "+threshold]), "sequence.sequenceID");
-	   });	
-	   
-	   myGeneAligns[featureName] = sufficientCoverageMembers;
-	   
-	   // Create the alignment in GLUE and add the members 
+		// Create the alignment name
+		var refSeqNameArray = refSeqName.split("_");
+		var virusSpecies = refSeqNameArray[1];
+		var geneAlignmentName = "AL_"+virusSpecies+"_"+featureName;
+		glue.logInfo("Creating gene alignment: "+geneAlignmentName);
 
-	   // Compute the alignment
-
+		// Create the alignment in GLUE and add the members
+		glue.command(["create", "alignment", geneAlignmentName, "-r", refSeqName]);
+		for(var i = 0; i < sufficientCoverageMembers.length; i++) {			
+			var seqID = sufficientCoverageMembers[i];			
+			glue.inMode("alignment/"+alignmentName+"/", function(){			
+				glue.command(["add", "member", "-w", "sequenceID = '"+seqID+"'"]);
+			});		   
+		}
+		
+		// Compute the alignment
+		glue.logInfo("  Computing alignment for feature: "+featureName+" in reference "+refSeqName);
+		glue.command(["compute", "alignment", alignmentName, "lentiCompoundAligner"]);
+	    
    }
    
-   mySpeciesAligns[refSeqName] = myGeneAligns;
 }	
 
 //### SUBROUTINES
@@ -80,6 +86,7 @@ function get_alignments(where_clause) {
 	
 		myArray.push(myMap);
 	}
+	glue.logInfo("Alignment result: "+myArray);
 
 	return myArray;
 }
@@ -93,11 +100,11 @@ function get_refseq_feature_locations(refSeqName) {
 	});
 
 	var listResult = listFeatureResult["listResult"];
-	glue.logInfo("Processing result: "+listResult);
+	//glue.logInfo("Processing result: "+listResult);
 	var rows = listResult["row"];
 	var columns = listResult["column"];
 	//glue.logInfo("Processing columns: "+columns);
-	glue.logInfo("Processing rows: "+rows);
+	//glue.logInfo("Processing rows: "+rows);
 
 	// transform result 
 	var myArray = [];
@@ -105,16 +112,19 @@ function get_refseq_feature_locations(refSeqName) {
 
 		var rowMap = rows[i];
 		var rowArray = rowMap["value"];
-		glue.logInfo("Processing row: "+rowArray);
+		//glue.logInfo("Processing row: "+rowArray);
 
 		for(var j = 0; j < columns.length; j++) {
 			var name = columns[j];
 			var rowValue = rowArray[j]		
-			glue.logInfo("Processing value: "+name+": "+rowValue);
-			myArray.push(rowValue);
+			//glue.logInfo("Processing value: "+name+": "+rowValue);
+			if (name == 'feature.name' && rowValue != 'whole_genome') {
+				myArray.push(rowValue);
+			}
 		}	
 	}
 
+	//glue.logInfo("Array RESULT!: "+myArray);
 	return myArray;
 }
 
