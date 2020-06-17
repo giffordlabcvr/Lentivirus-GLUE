f41ca3f67511d14db81b95ebd4bc728e298a51f5
Rob Gifford <robjgiff@gmail.com>
2020-06-17 19:22:07 +0100

    Refactoring

--- glue/core/lentiLoadRefseqsIsolateData2.js
+++ glue/core/lentiLoadRefseqsIsolateData2.js
@@ -1,7 +1,7 @@
 // Load EVE data from tab file 
 var loadResult;
 glue.inMode("module/lentiTabularUtility", function() {
-	loadResult = glue.tableToObjects(glue.command(["load-tabular", "tabular/refseq/lenti-reference-data.tsv"]));
+	loadResult = glue.tableToObjects(glue.command(["load-tabular", "tabular/core/lenti-reference-data.tsv"]));
 	//glue.log("INFO", "load result was:", loadResult);
 });
 
