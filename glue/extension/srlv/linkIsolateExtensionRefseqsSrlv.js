9a859a373fd8240620a3d032a6da4436fb2ef633
Rob Gifford <robjgiff@gmail.com>
2021-06-04 16:24:03 +0100

    Refactor

--- glue/extension/srlv/linkIsolateExtensionRefseqsSrlv.js
+++ glue/extension/srlv/linkIsolateExtensionRefseqsSrlv.js
@@ -52,30 +52,3 @@ _.each(seqIds, function(seqId) {
 
 });
 
-
-// list the sequences in source fasta-refseqs-concatenated-srlv
-var listSeqResult = glue.command(["list", "sequence", "-w", "source.name = 'fasta-refseqs-concatenated-srlv'"]);
-
-// extract from the result a list of sequence IDs.
-var seqIds = glue.getTableColumn(listSeqResult, "sequenceID");
-
-// for each sequence ID
-_.each(seqIds, function(seqId) {
-    
-    // create an object in the custom table which uses the sequence ID as the row ID.
-    glue.command(["create", "custom-table-row", "isolate_data", seqId]);
-    
-    // associate the corresponding sequence with this object.
-    glue.inMode("sequence/fasta-refseqs-concatenated-srlv/"+seqId, function() {
-        glue.command(["set", "link-target", "isolate_data", "custom-table-row/isolate_data/"+seqId]);
-    });
-
-	glue.inMode("sequence/fasta-refseqs-concatenated-srlv/"+seqId, function() {
-	
-		glue.command(["set", "field", "species", 'SRLV']);	
-		glue.command(["set", "field", "clade", 'Ovine-Caprine']);
-		
-	});
-
-});
-
