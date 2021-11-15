266497abdf2b717b421eb000b174742af43ede12
Rob Gifford <robjgiff@gmail.com>
2021-11-15 17:16:38 +0000

    Refactor SRLV projects

--- glue/build/extension/srlv/deleteCustomTableRows.js
+++ glue/build/extension/srlv/deleteCustomTableRows.js
@@ -1,22 +1,22 @@
 
 // Delete isolate table rows 
- var resultListRefseqs = glue.tableToObjects(glue.command(["list", "sequence", "sequenceID", "source.name", "-w", "source.name = 'ncbi-refseqs-srlv'"]));	 
+ var resultListRefseqs = glue.tableToObjects(glue.command(["list", "custom-table-row", "isolate_data"]));	 
+
  _.each(resultListRefseqs,function(resultObj) {
 
-	var sequenceID = resultObj["sequenceID"];
-	glue.log("INFO", "Deleting isolate data table for:", sequenceID);
-	glue.command(["delete", "custom-table-row", "isolate_data", sequenceID]);				
+	var sequenceID = resultObj["id"];
+	
+    var resultListSrlv = glue.tableToObjects(glue.command(["list", "sequence", "sequenceID", "source.name", "-w", "source.name like '%-srlv%'"]));	 
+    _.each(resultListSrlv,function(resultObj) {
 
-});
-
-
-// Delete isolate table rows 
- var resultListCurated = glue.tableToObjects(glue.command(["list", "sequence", "sequenceID", "source.name", "-w", "source.name = 'ncbi-curated-srlv'"]));	 
- _.each(resultListCurated,function(resultObj) {
-
-	var sequenceID = resultObj["sequenceID"];
-	glue.log("INFO", "Deleting isolate data table for:", sequenceID);
-	glue.command(["delete", "custom-table-row", "isolate_data", sequenceID]);				
-
+	   var srlvSequenceID = resultObj["sequenceID"];
+	   glue.log("INFO", "Got ID:", srlvSequenceID);
+	   if (srlvSequenceID == sequenceID) {
+   
+		 glue.log("INFO", "Deleting isolate data table for:", sequenceID);
+		 glue.command(["delete", "custom-table-row", "isolate_data", sequenceID]);				
+	   	   
+	   }	
+	});
 });
 
