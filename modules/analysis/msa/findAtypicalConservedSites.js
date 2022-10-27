225741ead9453832a72cb9f13dcd39d072276241
Rob Gifford <robjgiff@gmail.com>
2022-10-27 11:37:00 +0100

    Refactor

--- modules/analysis/msa/findAtypicalConservedSites.js
+++ modules/analysis/msa/findAtypicalConservedSites.js
@@ -6,7 +6,7 @@ function findAtypicalConservedSites(refsequence, alignment, feature) {
 	//glue.log("INFO", "RESULT WAS ", codingFeatures);
 	
     // Get alignment/constraining master reference pairs
-	var alignmentsResult = glue.command(["list","alignment"]);
+	var alignmentsResult = glue.command(["list","alignment","-w","name = 'AL_Equine'"]);
 	var listResult = alignmentsResult["listResult"];
 	//glue.log("INFO", "RESULT WAS ", listResult);
 	var alignmentsList = listResult["row"];
@@ -19,6 +19,7 @@ function findAtypicalConservedSites(refsequence, alignment, feature) {
 	// get aa frequencies
 	var aaFrequencies = { };
     getAaFrequencies(alignmentsList, codingFeatures, conRefCodingFeatureCodonLists, aaFrequencies);
+	glue.log("INFO", "RESULT WAS ", aaFrequencies);
 	die;
 	
     // Compile list of conserved sites
@@ -26,82 +27,6 @@ function findAtypicalConservedSites(refsequence, alignment, feature) {
 
 
 }
-
-
-// Get AA frequencies:
-// for all alignments, for each coding feature in constrain reference
-// get frequencies at all codon positions in all coding features
-function getAaFrequencies (alignmentsList, codingFeatures, conRefCodingFeatureCodonLists, aaFrequencyResults) {		  
-		  
-    // Iterate through the reference sequences
-	_.each(alignmentsList, function(alignmentObj) {
-
-		//glue.log("INFO", "RESULT WAS ", alignmentObj);
-		
-		// Get reference from reference-alignment pair map
-		var alignmentName = alignmentObj["value"][0];
-		var referenceName = alignmentObj["value"][2];
-		//glue.log("INFO", "Alignment name result was:", alignmentName);
-		//glue.log("INFO", "Reference name result was:", referenceName);
-				
-		// list all features annotated in this reference 
-		// GLUE COMMAND: reference [referenceName] list feature-location
-		var refseqFeatures;
-		var refseqFeatureLengths = { };
-		glue.inMode("/reference/"+referenceName, function() {
-
-			refseqFeatures = glue.tableToObjects(glue.command(["list", "feature-location"]));
-			glue.log("INFO", "RESULT WAS ", refseqFeatures);
-	   
-		});  
-
-		// iterate through features
-		_.each(refseqFeatures, function(featureObj) {
-		   
-			//glue.log("INFO", "RESULT WAS ", featureObj);		   
-			var featureResults = {};
-  
-			// get feature name sequence
-			var featureName = featureObj["feature.name"];
-
-			glue.log("INFO", "Feature name result was:", featureName);
-
-			if (codingFeatures[featureName]) {
-				
-				glue.log("INFO", "Coding feature name result was:", featureName);
-				
-				// Get the codon list for this feature
-				var aminoAcidResults = conRefCodingFeatureCodonLists[referenceName][featureName];
-	            //glue.log("INFO", "RESULT WAS ", aminoAcidResults);
-					
-				// Iterate through all positions
-				_.each(aminoAcidResults, function(codingPositionObj) {
-				
-				
-					// Get frequency at each position
-					glue.log("INFO", "RESULT WAS ", codingPositionObj);
-					die;
-
-					glue.inMode("/alignment/"+alignmentName, function() {
-
-						refseqFeatures = glue.tableToObjects(glue.command(["list", "feature-location"]));
-						glue.log("INFO", "RESULT WAS ", refseqFeatures);
-	   
-					});  
-
-				
-				});
-					  
-			
-			}
-  
-			// capture frequency
-
-		}); 
-	
-	});
-
-}		  
 
 
 // Get all coding features
@@ -197,4 +122,85 @@ function getConstrainingRefCodingFeatureCodonList (alignmentsList, codingFeature
 
 }
 
+
+// Get AA frequencies:
+// for all alignments, for each coding feature in constrain reference
+// get frequencies at all codon positions in all coding features
+function getAaFrequencies (alignmentsList, codingFeatures, conRefCodingFeatureCodonLists, aaFrequencyResults) {		  
+		  
+    // Iterate through the reference sequences
+	_.each(alignmentsList, function(alignmentObj) {
+
+		//glue.log("INFO", "RESULT WAS ", alignmentObj);
+		
+		// Get reference from reference-alignment pair map
+		var alignmentName = alignmentObj["value"][0];
+		var referenceName = alignmentObj["value"][2];
+		//glue.log("INFO", "Alignment name result was:", alignmentName);
+		//glue.log("INFO", "Reference name result was:", referenceName);
+				
+		// list all features annotated in this reference 
+		// GLUE COMMAND: reference [referenceName] list feature-location
+		var refseqFeatures;
+		var refseqFeatureLengths = { };
+		glue.inMode("/reference/"+referenceName, function() {
+
+			refseqFeatures = glue.tableToObjects(glue.command(["list", "feature-location"]));
+			glue.log("INFO", "RESULT WAS ", refseqFeatures);
+	   
+		});  
+
+		// iterate through features
+		_.each(refseqFeatures, function(featureObj) {
+		   
+			//glue.log("INFO", "RESULT WAS ", featureObj);		   
+			var featureResults = {};
+  
+			// get feature name sequence
+			var featureName = featureObj["feature.name"];
+
+			glue.log("INFO", "Feature name result was:", featureName);
+
+			if (codingFeatures[featureName]) {
+				
+				glue.log("INFO", "Coding feature name result was:", featureName);
+				
+				// Get the codon list for this feature
+				var aminoAcidResults = conRefCodingFeatureCodonLists[referenceName][featureName];
+	            //glue.log("INFO", "RESULT WAS ", aminoAcidResults);
+					
+				// Iterate through all positions
+				_.each(aminoAcidResults, function(codingPositionObj) {
+						
+					// Get frequency at each position
+					//glue.log("INFO", "RESULT WAS ", codingPositionObj);
+					var codonLabel = codingPositionObj["codonLabel"];
+					var codonNts = codingPositionObj["codonNts"];
+					var aminoAcid = codingPositionObj["aminoAcid"];
+
+					glue.inMode("/alignment/"+alignmentName, function() {
+
+					    var frequencyTable = glue.tableToObjects(glue.command(["amino-acid","frequency","-r",referenceName,"-f",featureName, "-l",codonLabel,codonLabel]));
+						glue.log("INFO", "RESULT WAS ", frequencyTable);
+						
+						// capture frequency
+						//aaFrequencyResults[referenceName][featureName][codonLabel] = frequencyTable;
+					
+					}); 
+									
+				});
+									  	
+			}		
+
+		}); 
+	
+	});
+
+}		  
+
+
+// Get conserved codon sites
+function getConservedCodonSites () {
+
+}
 
